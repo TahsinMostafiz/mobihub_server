03fe94f9c2fb3a186e1e7a392e14fdbd562b6570
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-11-30 17:18:25 +0600

    Booking data svae on server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,6 +13,50 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("MobiHub Server Running");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.b8fg4md.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    //Database Collections
+    const categoriesCollections = client
+      .db("mobiHub")
+      .collection("categoriesCollection");
+    const phonesCollections = client.db("mobiHub").collection("phones");
+    const bookingsCollections = client.db("mobiHub").collection("bookings");
+
+    // categories get api
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoriesCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        category_id: id,
+      };
+      const result = await phonesCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // bookings post api
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollections.insertOne(booking);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`MobiHub server running on port ${port}`);
